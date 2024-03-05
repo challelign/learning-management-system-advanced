3ca5e9d7a2855a0b80ce311faf5ebfdb0224559c
Challelign Tilahun <chalie2123@gmail.com>
2024-03-05 09:51:59 +0300

    slide auto play interval

--- components/dashboard-carousel.tsx
+++ components/dashboard-carousel.tsx
@@ -12,9 +12,9 @@ const DashboardCarousel = () => {
 					showStatus={false}
 					infiniteLoop
 					useKeyboardArrows
-					transitionTime={1000}
+					transitionTime={5000}
 					autoPlay // Add this line to enable automatic sliding
-					interval={3000} // Set the interval (in milliseconds) between each slide
+					interval={7000} // Set the interval (in milliseconds) between each slide
 				>
 					<div className="slide-holder">
 						<Image
